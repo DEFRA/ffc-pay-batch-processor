df8eab18a8ca53482d6890de1e5d03f004fe7820
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-08-02 10:49:12 +0100

    Tidy up part 2

--- app/parse-siti-payment-file/transform-headers.js
+++ app/parse-siti-payment-file/transform-headers.js
@@ -1,18 +1,16 @@
-const transformHeaders = (headerData) => {
-  return {
-    invoiceNumber: headerData[1],
-    requestInvoiceNumber: headerData[2],
-    claimId: headerData[3],
-    paymentType: headerData[4],
-    frn: headerData[5],
-    currency: headerData[6],
-    totalValue: parseFloat(headerData[7]),
-    deliveryBodyCode: headerData[8],
-    preferenceCurrency: headerData[9],
-    creatorId: headerData[10],
-    monthlyPaymentSchedule: headerData[11],
-    lines: []
-  }
-}
+const transformHeaders = (headerData) => ({
+  invoiceNumber: headerData[1],
+  requestInvoiceNumber: headerData[2],
+  claimId: headerData[3],
+  paymentType: headerData[4],
+  frn: headerData[5],
+  currency: headerData[6],
+  totalValue: parseFloat(headerData[7]),
+  deliveryBodyCode: headerData[8],
+  preferenceCurrency: headerData[9],
+  creatorId: headerData[10],
+  monthlyPaymentSchedule: headerData[11],
+  lines: []
+})
 
 module.exports = transformHeaders
