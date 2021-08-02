f904c2658431531df9a192d4e07e8378b245ac60
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-08-02 11:43:00 +0100

    Blob storage and tests

--- app/server.js
+++ app/server.js
@@ -1,14 +1,18 @@
 const Hapi = require('@hapi/hapi')
 
-const server = Hapi.server({
-  port: process.env.PORT
-})
+async function createServer () {
+  const server = Hapi.server({
+    port: process.env.PORT
+  })
 
-const routes = [].concat(
-  require('./routes/healthy'),
-  require('./routes/healthz')
-)
+  const routes = [].concat(
+    require('./routes/healthy'),
+    require('./routes/healthz')
+  )
 
-server.route(routes)
+  server.route(routes)
 
-module.exports = server
+  return server
+}
+
+module.exports = createServer
