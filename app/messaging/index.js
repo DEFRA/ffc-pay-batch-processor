ffb32fe545e93f76dab2f3b77398680b9cfe532a
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-08-03 13:54:09 +0100

    Remove console.log

--- app/messaging/index.js
+++ app/messaging/index.js
@@ -2,7 +2,6 @@ const sendMessage = require('./send-message')
 const config = require('../config/mq-config')
 
 async function sendPaymentBatchMessage (payload, correlationId) {
-  console.log(payload)
   await sendMessage(payload, 'uk.gov.sfi.payment.send', correlationId, config.paymentBatchTopic)
   console.info('Payment Sent')
 }
