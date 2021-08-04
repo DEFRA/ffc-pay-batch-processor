c493807d7a013c9bfb9a3200b54f898e7b928782
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-08-04 16:07:22 +0100

    Add database (#6)
    * Add database config and scrips
    * Add postgress set up to Helm chart
    * Add database changelog and models

---
+++ app/data/models/batch.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const batch = sequelize.define('batch', {
+    batchId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    filename: DataTypes.STRING,
+    sequenceNumber: DataTypes.INTEGER,
+    schemeId: DataTypes.INTEGER,
+    statusId: DataTypes.INTEGER,
+    processedOn: DataTypes.DATE,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  },
+  {
+    tableName: 'batches',
+    freezeTableName: true
+  })
+  batch.associate = function (models) {
+    batch.hasOne(models.status, {
+      foreignKey: 'statusId',
+      as: 'status'
+    })
+    batch.hasOne(models.scheme, {
+      foreignKey: 'schemeId',
+      as: 'scheme'
+    })
+  }
+  return batch
+}
