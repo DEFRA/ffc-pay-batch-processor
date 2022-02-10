ef53981fb7c2b38224ec8b7af340398242c0a179
John Watson <johnwatson484@hotmail.com>
2022-02-10 21:16:13 +0000

    Add request validation

--- app/parse-siti-payment-file/schemas/payment-request.js
+++ app/parse-siti-payment-file/schemas/payment-request.js
@@ -1,22 +1,17 @@
-const Joi = require('joi')
+const Joi = require('joi').extend(require('@joi/date'))
 
 module.exports = Joi.object({
-  sourceSystem: Joi.string(),
-  ledger: Joi.string(),
-  deliveryBody: Joi.string(),
-  invoiceNumber: Joi.string(),
-  frn: Joi.number().greater(1000000000).less(9999999999).required(),
-  sbi: Joi.number().integer().greater(105000000).less(999999999).optional(),
-  marketingYear: Joi.number().integer().greater(2020).less(2099).required(),
-  paymentRequestNumber: Joi.number().required(),
-  agreementNumber: Joi.string(),
-  contractNumber: Joi.string(),
-  currency: Joi.string(),
-  schedule: Joi.string(),
-  dueDate: Joi.string(),
-  debtType: Joi.string().allow(''),
-  recoveryDate: Joi.string().allow(''),
-  originalSettlementDate: Joi.string().allow(''),
+  sourceSystem: Joi.string().required(),
+  deliveryBody: Joi.string().regex(/^[A-Z]{2}\d{2}$/).required(),
+  invoiceNumber: Joi.string().regex(/^[A-Z]{3}\d{8}$/).required(),
+  frn: Joi.number().integer().min(1000000000).max(9999999999).required(),
+  marketingYear: Joi.number().integer().greater(2015).less(2099).required(),
+  paymentRequestNumber: Joi.number().integer().required(),
+  agreementNumber: Joi.string().regex(/^[A-Z]{3}\d{11}$/).required(),
+  contractNumber: Joi.string().regex(/^[A-Z]{4}\d{6}$/).required(),
+  currency: Joi.string().valid('GBP', 'EUR').required(),
+  schedule: Joi.string().regex(/^[A-Z]{1}\d+$/),
+  dueDate: Joi.date().format('YYYY-MM-DD'),
   value: Joi.number().required(),
   invoiceLines: Joi.array().required()
 })
