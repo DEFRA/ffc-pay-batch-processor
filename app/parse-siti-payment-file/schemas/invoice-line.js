ae1fa5bc0cada473fe982ade16a2c9606eb030c3
John Watson <johnwatson484@hotmail.com>
2022-02-10 21:28:07 +0000

    Add line split

--- app/parse-siti-payment-file/schemas/invoice-line.js
+++ app/parse-siti-payment-file/schemas/invoice-line.js
@@ -1,10 +1,9 @@
 const Joi = require('joi')
 
 module.exports = Joi.object({
-  standardCode: Joi.string().allow(''),
   schemeCode: Joi.string().required(),
-  accountCode: Joi.string().allow(''),
-  fundCode: Joi.string().required(),
-  description: Joi.string().required(),
+  accountCode: Joi.string().regex(/^[A-Z]{3}\d{3}$/).required(),
+  fundCode: Joi.string().regex(/^[A-Z]{3}\d{2}$/).required(),
+  description: Joi.string().regex(/^[A-Z]{1}\d{2}\s-\s.+$/).required(),
   value: Joi.number().required()
 })
