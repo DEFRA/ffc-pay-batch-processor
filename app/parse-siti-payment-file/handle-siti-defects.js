325a8bf34dbfce7f30dfe632263ed0b9dd359e5b
John Watson <johnwatson484@hotmail.com>
2022-04-07 09:12:20 +0100

    Add Siti defect module

---
+++ app/parse-siti-payment-file/handle-siti-defects.js
@@ -0,0 +1,22 @@
+const GROSS_LINE_DESCRIPTION = 'G00 - Gross value of claim'
+const PARTICIPATION_PAYMENT_SCHEME_CODE = '80009'
+
+const handleSitiDefects = (paymentRequest) => {
+  return removeDefunctParticipationPayment(paymentRequest)
+}
+
+const removeDefunctParticipationPayment = (paymentRequest) => {
+  // Defect in Siti Agri where full agreement should be recovered, but Siti Agri cannot set the participation gross value to 0
+  // If participation payment is only gross value, then we need to set it to 0 so Delta will calculate correctly
+  const nonParticipationPaymentInvoiceLines = paymentRequest.invoiceLines.filter(x => x.description === GROSS_LINE_DESCRIPTION && x.value !== 0 && x.schemeCode !== PARTICIPATION_PAYMENT_SCHEME_CODE)
+  if (nonParticipationPaymentInvoiceLines.length) {
+    const participationPaymentInvoiceLine = paymentRequest.invoiceLines.find(x => x.description === GROSS_LINE_DESCRIPTION && x.value !== 0 && x.schemeCode === PARTICIPATION_PAYMENT_SCHEME_CODE)
+    if (participationPaymentInvoiceLine) {
+      paymentRequest.value = paymentRequest.value - participationPaymentInvoiceLine.value
+      participationPaymentInvoiceLine.value = 0
+    }
+  }
+  return paymentRequest
+}
+
+module.exports = handleSitiDefects
