73a35b96a2b316476f971895e172a9d6d85d978a
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-05-10 15:32:17 +0100

    Add batchExportDate to the event

--- app/event/send-batch-processing-events.js
+++ app/event/send-batch-processing-events.js
@@ -1,12 +1,12 @@
 const raiseEventBatch = require('./raise-event-batch')
 
-const sendBatchProcessedEvents = async (filename, paymentRequests, sequence) => {
+const sendBatchProcessedEvents = async (filename, paymentRequests, sequence, batchExportDate) => {
   const events = paymentRequests.map(paymentRequest => ({
     id: paymentRequest.correlationId,
     name: 'batch-processing',
     type: 'info',
     message: 'Payment request created from batch file',
-    data: { filename, sequence, paymentRequest }
+    data: { filename, sequence, paymentRequest, batchExportDate }
   }))
   await raiseEventBatch(events)
 }
