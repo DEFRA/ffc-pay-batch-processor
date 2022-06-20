ebef85786dae074e5cd72cbf8de80cf0d558f622
Marc Templeton <marcte@kainos.com>
2022-06-20 12:59:25 +0100

    fixing import

--- app/process-batches/quarantine-file.js
+++ app/process-batches/quarantine-file.js
@@ -3,7 +3,7 @@ const sendBatchQuarantineEvent = require('../event/send-batch-quarantine-event')
 
 const quarantineFile = async (filename, error) => {
   console.error(`Quarantining ${filename}, failed to parse file`, error)
-  await blobStorage.quarantineFile(filename, filename)
+  await blobStorage.quarantinePaymentFile(filename, filename)
   await sendBatchQuarantineEvent(filename, error)
 }
 
