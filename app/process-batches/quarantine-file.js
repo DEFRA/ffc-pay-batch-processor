c9695d15ea7fc45fecb7dc9aa3fb86df2064a786
Marc Templeton <marcte@kainos.com>
2022-06-22 16:26:35 +0100

    removing error from signature

--- app/process-batches/quarantine-file.js
+++ app/process-batches/quarantine-file.js
@@ -1,9 +1,9 @@
 const blobStorage = require('../blob-storage')
 const sendBatchQuarantineEvent = require('../event/send-batch-quarantine-event')
 
-const quarantineFile = async (filename, error) => {
-  console.error(`Quarantining ${filename}, failed to parse file`, error)
-  await sendBatchQuarantineEvent(filename, error)
+const quarantineFile = async (filename) => {
+  console.error(`Quarantining ${filename}, failed to parse file`)
+  await sendBatchQuarantineEvent(filename)
   return blobStorage.quarantinePaymentFile(filename, filename)
 }
 
