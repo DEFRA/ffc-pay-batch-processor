932490e109e8b479106af31e55c3871df4640993
Marc Templeton <marcte@kainos.com>
2022-06-22 14:51:56 +0100

    removing redundant error param

--- app/event/send-batch-quarantine-event.js
+++ app/event/send-batch-quarantine-event.js
@@ -1,15 +1,14 @@
 const { v4: uuidv4 } = require('uuid')
 const raiseEvent = require('./raise-event')
 
-const sendBatchQuarantineEvent = async (filename, error) => {
+const sendBatchQuarantineEvent = async (filename) => {
   const event = {
     id: uuidv4(),
     name: 'batch-processing-quarantine-error',
     type: 'error',
     message: `Quarantined ${filename}`,
     data: {
-      filename,
-      error
+      filename
     }
   }
   await raiseEvent(event, 'error')
