50636aea1d56676bc1f3030086a0f8be7d427665
Marc Templeton <marcte@kainos.com>
2022-06-22 17:12:59 +0100

    updating tests

--- test/unit/event/send-batch-quarantine-event.test.js
+++ test/unit/event/send-batch-quarantine-event.test.js
@@ -9,7 +9,7 @@ const sendBatchQuarantineEvent = require('../../../app/event/send-batch-quaranti
 let filename
 let event
 
-describe('Sending event for quarantined SITI payment file', () => {
+describe('Sending event for quarantined DAX response file', () => {
   beforeEach(async () => {
     uuidv4.mockImplementation(() => { '70cb0f07-e0cf-449c-86e8-0344f2c6cc6c' })
 
@@ -39,15 +39,53 @@ describe('Sending event for quarantined SITI payment file', () => {
     expect(uuidv4).toHaveBeenCalledTimes(1)
   })
 
-  test('should call uuidv4 when no filename is received', async () => {
-    filename = ''
-    await sendBatchQuarantineEvent(filename)
+  test('should call uuidv4 when an empty string is received', async () => {
+    await sendBatchQuarantineEvent('')
     expect(uuidv4).toHaveBeenCalled()
   })
 
-  test('should call uuidv4 once when no filename is received', async () => {
-    filename = ''
-    await sendBatchQuarantineEvent(filename)
+  test('should call uuidv4 once when an empty string is received', async () => {
+    await sendBatchQuarantineEvent('')
+    expect(uuidv4).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call uuidv4 when an object is received', async () => {
+    await sendBatchQuarantineEvent({})
+    expect(uuidv4).toHaveBeenCalled()
+  })
+
+  test('should call uuidv4 once when an object is received', async () => {
+    await sendBatchQuarantineEvent({})
+    expect(uuidv4).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call uuidv4 when an array is received', async () => {
+    await sendBatchQuarantineEvent([])
+    expect(uuidv4).toHaveBeenCalled()
+  })
+
+  test('should call uuidv4 once when an array is received', async () => {
+    await sendBatchQuarantineEvent([])
+    expect(uuidv4).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call uuidv4 when undefined is received', async () => {
+    await sendBatchQuarantineEvent(undefined)
+    expect(uuidv4).toHaveBeenCalled()
+  })
+
+  test('should call uuidv4 once when undefined is received', async () => {
+    await sendBatchQuarantineEvent(undefined)
+    expect(uuidv4).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call uuidv4 when null is received', async () => {
+    await sendBatchQuarantineEvent(null)
+    expect(uuidv4).toHaveBeenCalled()
+  })
+
+  test('should call uuidv4 once when null is received', async () => {
+    await sendBatchQuarantineEvent(null)
     expect(uuidv4).toHaveBeenCalledTimes(1)
   })
 
@@ -68,34 +106,137 @@ describe('Sending event for quarantined SITI payment file', () => {
     }
 
     await sendBatchQuarantineEvent(filename)
+
     expect(raiseEvent).toHaveBeenCalledWith(event, 'error')
   })
 
-  test('should call raiseEvent when no filename is received', async () => {
-    filename = ''
-    await sendBatchQuarantineEvent(filename)
+  test('should call raiseEvent when an empty string is received', async () => {
+    await sendBatchQuarantineEvent('')
     expect(raiseEvent).toHaveBeenCalled()
   })
 
-  test('should call raiseEvent once when no filename is received', async () => {
-    filename = ''
-    await sendBatchQuarantineEvent(filename)
+  test('should call raiseEvent once when an empty string is received', async () => {
+    await sendBatchQuarantineEvent('')
     expect(raiseEvent).toHaveBeenCalledTimes(1)
   })
 
-  test('should call raiseEvent with event and "error" when no filename is received', async () => {
+  test('should call raiseEvent with event and "error" when an empty string is received', async () => {
     filename = ''
     event = {
       ...event,
-      message: `Quarantined ${filename}`,
       id: uuidv4(),
+      message: `Quarantined ${filename}`,
       data: {
-        ...event.data,
         filename
       }
     }
 
     await sendBatchQuarantineEvent(filename)
+
+    expect(raiseEvent).toHaveBeenCalledWith(event, 'error')
+  })
+
+  test('should call raiseEvent when an object is received', async () => {
+    await sendBatchQuarantineEvent({})
+    expect(raiseEvent).toHaveBeenCalled()
+  })
+
+  test('should call raiseEvent once when an object is received', async () => {
+    await sendBatchQuarantineEvent({})
+    expect(raiseEvent).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call raiseEvent with event and "error" when an object is received', async () => {
+    filename = {}
+    event = {
+      ...event,
+      id: uuidv4(),
+      message: `Quarantined ${filename}`,
+      data: {
+        filename
+      }
+    }
+
+    await sendBatchQuarantineEvent(filename)
+
+    expect(raiseEvent).toHaveBeenCalledWith(event, 'error')
+  })
+
+  test('should call raiseEvent when an array is received', async () => {
+    await sendBatchQuarantineEvent([])
+    expect(raiseEvent).toHaveBeenCalled()
+  })
+
+  test('should call raiseEvent once when an array is received', async () => {
+    await sendBatchQuarantineEvent([])
+    expect(raiseEvent).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call raiseEvent with event and "error" when an array is received', async () => {
+    filename = []
+    event = {
+      ...event,
+      id: uuidv4(),
+      message: `Quarantined ${filename}`,
+      data: {
+        filename
+      }
+    }
+
+    await sendBatchQuarantineEvent(filename)
+
+    expect(raiseEvent).toHaveBeenCalledWith(event, 'error')
+  })
+
+  test('should call raiseEvent when undefined is received', async () => {
+    await sendBatchQuarantineEvent(undefined)
+    expect(raiseEvent).toHaveBeenCalled()
+  })
+
+  test('should call raiseEvent once when undefined is received', async () => {
+    await sendBatchQuarantineEvent(undefined)
+    expect(raiseEvent).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call raiseEvent with event and "error" when undefined is received', async () => {
+    filename = undefined
+    event = {
+      ...event,
+      id: uuidv4(),
+      message: `Quarantined ${filename}`,
+      data: {
+        filename
+      }
+    }
+
+    await sendBatchQuarantineEvent(filename)
+
+    expect(raiseEvent).toHaveBeenCalledWith(event, 'error')
+  })
+
+  test('should call raiseEvent when null is received', async () => {
+    await sendBatchQuarantineEvent(null)
+    expect(raiseEvent).toHaveBeenCalled()
+  })
+
+  test('should call raiseEvent once when null is received', async () => {
+    await sendBatchQuarantineEvent(null)
+    expect(raiseEvent).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call raiseEvent with event and "error" when null is received', async () => {
+    filename = null
+    event = {
+      ...event,
+      id: uuidv4(),
+      message: `Quarantined ${filename}`,
+      data: {
+        filename
+      }
+    }
+
+    await sendBatchQuarantineEvent(filename)
+
     expect(raiseEvent).toHaveBeenCalledWith(event, 'error')
   })
 })
