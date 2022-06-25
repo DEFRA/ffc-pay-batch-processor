a6007a5c25d80a9467c69485353ad5cfc14e20a7
John Watson <johnwatson484@hotmail.com>
2022-06-25 19:07:38 +0100

    Add tests

--- test/integration/processing.test.js
+++ test/integration/processing.test.js
@@ -42,6 +42,11 @@ const TEST_FILEPATH_LUMP_SUMS = path.resolve(__dirname, '../files', TEST_FILE_LU
 const TEST_INVALID_FILE_LUMP_SUMS = 'SITILSES_0001_AP_20220624212100914.dat'
 const TEST_INVALID_FILEPATH_LUMP_SUMS = path.resolve(__dirname, '../files', TEST_INVALID_FILE_LUMP_SUMS)
 
+const TEST_FILE_SFI = 'SITISFI0001_AP_20210812105407545.dat'
+const TEST_FILEPATH_SFI = path.resolve(__dirname, '../files', TEST_FILE_SFI)
+const TEST_INVALID_FILE_SFI = 'SITISFI0001_AP_20210812105407546.dat'
+const TEST_INVALID_FILEPATH_SFI = path.resolve(__dirname, '../files', TEST_INVALID_FILE_SFI)
+
 describe('process acknowledgement', () => {
   beforeEach(async () => {
     jest.clearAllMocks()
@@ -192,5 +197,61 @@ describe('process acknowledgement', () => {
       fileList.push(item.name)
     }
     expect(fileList.filter(x => x === `${config.quarantineFolder}/${TEST_INVALID_FILE_LUMP_SUMS}`).length).toBe(1)
+  })
+
+  test('sends all payment requests for SFI', async () => {
+    const blockBlobClient = container.getBlockBlobClient(`${config.inboundFolder}/${TEST_FILE_SFI}`)
+    await blockBlobClient.uploadFile(TEST_FILEPATH_SFI)
+    await pollInbound()
+    expect(mockSendBatchMessages.mock.calls[0][0].length).toBe(2)
+  })
+
+  test('sends invoice numbers for SFI', async () => {
+    const blockBlobClient = container.getBlockBlobClient(`${config.inboundFolder}/${TEST_FILE_SFI}`)
+    await blockBlobClient.uploadFile(TEST_FILEPATH_SFI)
+    await pollInbound()
+    expect(mockSendBatchMessages.mock.calls[0][0][0].body.invoiceNumber).toBe('SFIP0000001')
+    expect(mockSendBatchMessages.mock.calls[0][0][1].body.invoiceNumber).toBe('SFIP0000002')
+  })
+
+  test('sends payment request numbers for SFI', async () => {
+    const blockBlobClient = container.getBlockBlobClient(`${config.inboundFolder}/${TEST_FILE_SFI}`)
+    await blockBlobClient.uploadFile(TEST_FILEPATH_SFI)
+    await pollInbound()
+    expect(mockSendBatchMessages.mock.calls[0][0][0].body.paymentRequestNumber).toBe(1)
+    expect(mockSendBatchMessages.mock.calls[0][0][1].body.paymentRequestNumber).toBe(3)
+  })
+
+  test('archives file on success for SFI', async () => {
+    const blockBlobClient = container.getBlockBlobClient(`${config.inboundFolder}/${TEST_FILE_SFI}`)
+    await blockBlobClient.uploadFile(TEST_FILEPATH_SFI)
+    await pollInbound()
+    const fileList = []
+    for await (const item of container.listBlobsFlat({ prefix: config.archiveFolder })) {
+      fileList.push(item.name)
+    }
+    expect(fileList.filter(x => x === `${config.archiveFolder}/${TEST_FILE_SFI}`).length).toBe(1)
+  })
+
+  test('ignores unrelated file for SFI', async () => {
+    const blockBlobClient = container.getBlockBlobClient(`${config.inbound}/ignore me.dat`)
+    await blockBlobClient.uploadFile(TEST_FILEPATH_SFI)
+    await pollInbound()
+    const fileList = []
+    for await (const item of container.listBlobsFlat()) {
+      fileList.push(item.name)
+    }
+    expect(fileList.filter(x => x === `${config.inbound}/ignore me.dat`).length).toBe(1)
+  })
+
+  test('quarantines invalid file for SFI', async () => {
+    const blockBlobClient = container.getBlockBlobClient(`${config.inboundFolder}/${TEST_INVALID_FILE_SFI}`)
+    await blockBlobClient.uploadFile(TEST_INVALID_FILEPATH_SFI)
+    await pollInbound()
+    const fileList = []
+    for await (const item of container.listBlobsFlat({ prefix: config.quarantineFolder })) {
+      fileList.push(item.name)
+    }
+    expect(fileList.filter(x => x === `${config.quarantineFolder}/${TEST_INVALID_FILE_SFI}`).length).toBe(1)
   })
 })
