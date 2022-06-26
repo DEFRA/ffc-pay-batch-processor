2b476a8ffbe7cfdaa26e95e23355829a2b9e2c27
John Watson <johnwatson484@hotmail.com>
2022-06-26 17:09:39 +0100

    Fix typo

--- test/unit/processing/siti-agri/build-invoice-lines.test.js
+++ test/unit/processing/siti-agri/build-invoice-lines.test.js
@@ -39,28 +39,28 @@ describe('Build invoice lines', () => {
     expect(invoiceLineIsValid).toBe(true)
   })
 
-  test('Falied validation of invoice lines for schemeCode', async () => {
+  test('Failed validation of invoice lines for schemeCode', async () => {
     invoiceLines[0].schemeCode = 123
     const invoiceLineIsValid = isInvoiceLineValid(invoiceLines[0])
     expect(console.error).toHaveBeenLastCalledWith('Invoice line is invalid. "schemeCode" must be a string')
     expect(invoiceLineIsValid).toBe(false)
   })
 
-  test('Falied validation of invoice lines for accountCode', async () => {
+  test('Failed validation of invoice lines for accountCode', async () => {
     invoiceLines[0].accountCode = 'ABC'
     const invoiceLineIsValid = isInvoiceLineValid(invoiceLines[0])
     expect(console.error).toHaveBeenLastCalledWith('Invoice line is invalid. "accountCode" with value "ABC" fails to match the required pattern: /^[A-Z]{3}\\d{3}$/')
     expect(invoiceLineIsValid).toBe(false)
   })
 
-  test('Falied validation of invoice lines for fundCode', async () => {
+  test('Failed validation of invoice lines for fundCode', async () => {
     invoiceLines[0].fundCode = 'ABC'
     const invoiceLineIsValid = isInvoiceLineValid(invoiceLines[0])
     expect(console.error).toHaveBeenLastCalledWith('Invoice line is invalid. "fundCode" with value "ABC" fails to match the required pattern: /^[A-Z]{3}\\d{2}$/')
     expect(invoiceLineIsValid).toBe(false)
   })
 
-  test('Falied validation of invoice lines for description', async () => {
+  test('Failed validation of invoice lines for description', async () => {
     invoiceLines[0].description = 'Gross value of claim'
     const invoiceLineIsValid = isInvoiceLineValid(invoiceLines[0])
     expect(console.error).toHaveBeenLastCalledWith('Invoice line is invalid. "description" with value "Gross value of claim" fails to match the required pattern: /^[A-Z]{1}\\d{2}\\s-\\s.+$/')
