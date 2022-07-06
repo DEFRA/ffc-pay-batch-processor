b2afcda4e6983b35b815f81889d55be7c6185429
Marc Templeton <marcte@kainos.com>
2022-07-06 11:52:03 +0100

    added mock batch header object and modified validate batch test

---
+++ test/mockBatchHeader.js
@@ -0,0 +1,13 @@
+const { AP } = require('../app/ledgers')
+const { sfiPilot } = require('../app/schemes')
+
+const batchHeader = {
+  batchValue: 100,
+  exportDate: '2022-06-28',
+  ledger: AP,
+  numberOfPaymentRequests: 1,
+  sequence: 1,
+  sourceSystem: sfiPilot.sourceSystem
+}
+
+module.exports = batchHeader
--- test/unit/processing/siti-agri/validate-batch.test.js
+++ test/unit/processing/siti-agri/validate-batch.test.js
@@ -1,6 +1,3 @@
-const { AP } = require('../../../../app/ledgers')
-const { sfi } = require('../../../../app/schemes')
-
 jest.mock('../../../../app/currency-convert')
 const { convertToPence, getTotalValueInPence } = require('../../../../app/currency-convert')
 
@@ -11,22 +8,8 @@ let paymentRequest
 
 describe('Validate batch', () => {
   beforeEach(() => {
-    batchHeader = {
-      exportDate: '2022-06-28',
-      numberOfPaymentRequests: 1,
-      batchValue: 100,
-      sequence: 1,
-      sourceSystem: sfi.sourceSystem,
-      ledger: AP
-    }
-    paymentRequest = {
-      value: 100,
-      invoiceLines: [{
-        value: 50
-      }, {
-        value: 50
-      }]
-    }
+    batchHeader = require('../../../mockBatchHeader')
+    paymentRequest = require('../../../mockPaymentRequest').paymentRequest
 
     convertToPence.mockImplementation(() => batchHeader.batchValue)
     getTotalValueInPence.mockImplementation(() => paymentRequest.value)
@@ -48,110 +31,200 @@ describe('Validate batch', () => {
   })
 
   test('returns false if batch value missing', async () => {
-    batchHeader.batchValue = undefined
+    batchHeader = {
+      ...batchHeader,
+      batchValue: undefined
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false if expected number of payment requests is less than actual', async () => {
-    batchHeader.numberOfPaymentRequests = 0
+    batchHeader = {
+      ...batchHeader,
+      numberOfPaymentRequests: 0
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false if expected number of payment requests is more than actual', async () => {
-    batchHeader.numberOfPaymentRequests = 2
+    batchHeader = {
+      ...batchHeader,
+      numberOfPaymentRequests: 2
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false if unknown ledger', async () => {
-    batchHeader.ledger = 'unknown'
+    batchHeader = {
+      ...batchHeader,
+      ledger: 'unknown'
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false if ledger missing', async () => {
-    batchHeader.ledger = undefined
+    batchHeader = {
+      ...batchHeader,
+      ledger: undefined
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false if export date DD-MM-YYYY', async () => {
-    batchHeader.exportDate = '28-06-2022'
+    batchHeader = {
+      ...batchHeader,
+      exportDate: '28-06-2022'
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false if export date DD/MM/YYYY', async () => {
-    batchHeader.exportDate = '28/06/2022'
+    batchHeader = {
+      ...batchHeader,
+      exportDate: '28/06/2022'
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false if export date YYYY/MM/DD', async () => {
-    batchHeader.exportDate = '2022/06/28'
+    batchHeader = {
+      ...batchHeader,
+      exportDate: '2022/06/28'
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false if export date missing', async () => {
-    batchHeader.exportDate = undefined
+    batchHeader = {
+      ...batchHeader,
+      exportDate: undefined
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false if sequence is undefined', async () => {
-    batchHeader.sequence = undefined
+    batchHeader = {
+      ...batchHeader,
+      sequence: undefined
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false if sequence is a negative number', async () => {
-    batchHeader.sequence = -3
+    batchHeader = {
+      ...batchHeader,
+      sequence: -3
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false if sequence is a float', async () => {
-    batchHeader.sequence = 3.2
+    batchHeader = {
+      ...batchHeader,
+      sequence: 3.2
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false if sourceSystem is undefined', async () => {
-    batchHeader.sourceSystem = undefined
+    batchHeader = {
+      ...batchHeader,
+      sourceSystem: undefined
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns true when batch header value matches payment request value', async () => {
-    batchHeader.batchValue = paymentRequest.value
+    batchHeader = {
+      ...batchHeader,
+      batchValue: paymentRequest.value
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeTruthy()
   })
 
   test('returns false when batch header value is more than payment request value', async () => {
-    batchHeader.batchValue = paymentRequest.value + 50
+    batchHeader = {
+      ...batchHeader,
+      batchValue: paymentRequest.value + 50
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false when batch header value is less than payment request value', async () => {
-    batchHeader.batchValue = paymentRequest.value - 50
+    batchHeader = {
+      ...batchHeader,
+      batchValue: paymentRequest.value - 50
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false when payment request value is more than batch header value', async () => {
-    paymentRequest.value = batchHeader.batchValue + 50
+    paymentRequest = {
+      ...paymentRequest,
+      value: batchHeader.batchValue + 50
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 
   test('returns false when payment request value is less than batch header value', async () => {
-    paymentRequest.value = batchHeader.batchValue - 50
+    paymentRequest = {
+      ...paymentRequest,
+      value: batchHeader.batchValue - 50
+    }
+
     const result = await validateBatch([batchHeader], [paymentRequest])
+
     expect(result).toBeFalsy()
   })
 })
