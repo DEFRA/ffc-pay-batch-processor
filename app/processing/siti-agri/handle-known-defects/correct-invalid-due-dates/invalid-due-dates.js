0fd59878c72b7149c5ac22aa6f9075834bd43c96
John Watson <johnwatson484@hotmail.com>
2022-08-31 10:15:36 +0100

    Update due date issue to use 5th of month

--- app/processing/siti-agri/handle-known-defects/correct-invalid-due-dates/invalid-due-dates.js
+++ app/processing/siti-agri/handle-known-defects/correct-invalid-due-dates/invalid-due-dates.js
@@ -1,28 +1,28 @@
 module.exports = [{
-  invalid: '2022-07-01',
+  invalid: '2022-07-05',
   correction: '2022-10-05'
 }, {
-  invalid: '2022-08-01',
+  invalid: '2022-08-05',
   correction: '2022-11-05'
 }, {
-  invalid: '2022-09-01',
+  invalid: '2022-09-05',
   correction: '2022-12-05'
 }, {
-  invalid: '2022-10-01',
+  invalid: '2022-10-05',
   correction: '2023-01-05'
 }, {
-  invalid: '2022-11-01',
+  invalid: '2022-11-05',
   correction: '2023-02-05'
 }, {
-  invalid: '2022-12-01',
+  invalid: '2022-12-05',
   correction: '2023-03-05'
 }, {
-  invalid: '2023-01-01',
+  invalid: '2023-01-05',
   correction: '2023-04-05'
 }, {
-  invalid: '2023-02-01',
+  invalid: '2023-02-05',
   correction: '2023-05-05'
 }, {
-  invalid: '2023-03-01',
+  invalid: '2023-03-05',
   correction: '2023-06-05'
 }]
