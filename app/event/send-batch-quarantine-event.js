37347692c00110bde511630e3619e208a20b48b6
John Watson <johnwatson484@hotmail.com>
2023-02-17 21:23:34 +0000

    Add batch q event

--- app/event/send-batch-quarantine-event.js
+++ app/event/send-batch-quarantine-event.js
@@ -1,7 +1,18 @@
 const { v4: uuidv4 } = require('uuid')
 const raiseEvent = require('./raise-event')
+const config = require('../config/processing')
+const { EventPublisher } = require('ffc-pay-event-publisher')
 
 const sendBatchQuarantineEvent = async (filename) => {
+  if (config.useV1Events) {
+    await sendV1BatchQuarantineEvent(filename)
+  }
+  if (config.useV2Events) {
+    await sendV2BatchQuarantineEvent(filename)
+  }
+}
+
+const sendV1BatchQuarantineEvent = async (filename) => {
   const event = {
     id: uuidv4(),
     name: 'batch-processing-quarantine-error',
@@ -12,6 +23,20 @@ const sendBatchQuarantineEvent = async (filename) => {
     }
   }
   await raiseEvent(event, 'error')
+}
+
+const sendV2BatchQuarantineEvent = async (filename) => {
+  const event = {
+    source: 'ffc-pay-batch-processor',
+    type: 'uk.gov.defra.ffc.pay.warning.batch.quarantined',
+    subject: filename,
+    data: {
+      message: 'File quarantined',
+      filename
+    }
+  }
+  const eventPublisher = new EventPublisher(config.eventsTopic)
+  await eventPublisher.publishEvent(event)
 }
 
 module.exports = sendBatchQuarantineEvent
