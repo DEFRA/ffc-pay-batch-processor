f1cfc35241c58ea280a4b888ec80bba55099e250
John Watson <johnwatson484@hotmail.com>
2023-02-19 19:40:41 +0000

    Add batch

--- app/processing/siti-agri/build-payment-requests.js
+++ app/processing/siti-agri/build-payment-requests.js
@@ -7,6 +7,7 @@ const buildPaymentRequests = (paymentRequests, sourceSystem) => {
 
   return paymentRequests.map(paymentRequest => ({
     sourceSystem,
+    batch: paymentRequest.batch
     deliveryBody: paymentRequest.deliveryBody,
     invoiceNumber: paymentRequest.invoiceNumber,
     frn: paymentRequest.frn,
--- app/processing/siti-agri/schemas/batch-header.js
+++ app/processing/siti-agri/schemas/batch-header.js
@@ -3,6 +3,7 @@ const { AP, AR } = require('../../../ledgers')
 const Joi = require('joi').extend(require('@joi/date'))
 
 module.exports = Joi.object({
+  batch: Joi.string().required(),
   exportDate: Joi.date().format('YYYY-MM-DD').required(),
   numberOfPaymentRequests: Joi.number().required(),
   batchValue: Joi.number().required(),
