ffb7259b7ccc4e789254933559eb8563f32c5e9d
Simon Dunn <simondunn90@gmail.com>
2023-03-15 08:56:00 +0000

    update transform-header tests

--- test/unit/processing/siti-agri/transform-header.test.js
+++ test/unit/processing/siti-agri/transform-header.test.js
@@ -1,7 +1,7 @@
 const { GBP } = require('../../../../app/constants/currency')
 const transformHeader = require('../../../../app/processing/siti-agri/transform-header')
-const { M12 } = require('../../../../app/constants/schedules')
-const { sfi, sfiPilot, lumpSums } = require('../../../../app/schemes')
+const { M12 } = require('../../../../app/constants/schedule')
+const { sfi, sfiPilot, lumpSums, bps, cs } = require('../../../../app/schemes')
 
 jest.mock('uuid')
 const { v4: uuidv4 } = require('uuid')
@@ -11,9 +11,12 @@ describe('Transform header', () => {
   uuidv4.mockReturnValue(correlationId)
 
   test('transforms SFI header', async () => {
+    const filename = 'SITISFI0001_AP_20230315083522081.dat'
     const headerData = ['H', 'SFI0000001', '01', 'S000001', '1', '1000000001', 'GBP', '100', 'RP00', 'GBP', 'SFI', 'M12']
-    const result = transformHeader(headerData, sfi.schemeId)
-    expect(result).toMatchObject({
+    const result = transformHeader(headerData, sfi.schemeId, filename)
+    expect(result).toEqual({
+      correlationId,
+      batch: filename,
       invoiceNumber: 'SFI0000001',
       paymentRequestNumber: 1,
       contractNumber: 'S000001',
@@ -27,9 +30,12 @@ describe('Transform header', () => {
   })
 
   test('transforms SFI Pilot header', async () => {
+    const filename = 'SITIELM0001_AP_20230315083940939.dat'
     const headerData = ['H', 'SFIP0000001', '01', 'S000001', '1', '1000000001', 'GBP', '100', 'RP00', 'GBP', 'SFI', 'M12']
-    const result = transformHeader(headerData, sfiPilot.schemeId)
-    expect(result).toMatchObject({
+    const result = transformHeader(headerData, sfiPilot.schemeId, filename)
+    expect(result).toEqual({
+      correlationId,
+      batch: filename,
       invoiceNumber: 'SFIP0000001',
       paymentRequestNumber: 1,
       contractNumber: 'S000001',
@@ -43,9 +49,12 @@ describe('Transform header', () => {
   })
 
   test('transforms Lump Sums header', async () => {
+    const filename = 'SITILSES0001_AP_20230315084137333.dat'
     const headerData = ['H', 'LSES0000001', '001', 'L000001', '1000000001', '1', '100', 'RP00', 'GBP']
-    const result = transformHeader(headerData, lumpSums.schemeId)
-    expect(result).toMatchObject({
+    const result = transformHeader(headerData, lumpSums.schemeId, filename)
+    expect(result).toEqual({
+      correlationId,
+      batch: filename,
       invoiceNumber: 'LSES0000001',
       paymentRequestNumber: 1,
       contractNumber: 'L000001',
@@ -53,6 +62,42 @@ describe('Transform header', () => {
       currency: GBP,
       value: 100,
       deliveryBody: 'RP00',
+      invoiceLines: []
+    })
+  })
+
+  test('transforms BPS header', async () => {
+    const filename = 'SITI_0001_AP_20230315081841316.dat'
+    const headerData = ['H', 'SITI0000001', '001', 'C0000001', '1000000001', '1', '100', 'RP00', 'GBP']
+    const result = transformHeader(headerData, bps.schemeId, filename)
+    expect(result).toEqual({
+      correlationId,
+      batch: filename,
+      invoiceNumber: 'SITI0000001',
+      paymentRequestNumber: 1,
+      contractNumber: 'C0000001',
+      frn: '1000000001',
+      value: 100,
+      deliveryBody: 'RP00',
+      currency: GBP,
+      invoiceLines: []
+    })
+  })
+
+  test('transforms CS header', async () => {
+    const filename = 'SITICS0001_AP_20230315084313836.dat'
+    const headerData = ['H', 'CS000000001', '001', 'A0000001', '1', '1000000001', 'GBP', '100', 'NE00', 'GBP']
+    const result = transformHeader(headerData, cs.schemeId, filename)
+    expect(result).toEqual({
+      correlationId,
+      batch: filename,
+      invoiceNumber: 'CS000000001',
+      paymentRequestNumber: 1,
+      contractNumber: 'A0000001',
+      frn: '1000000001',
+      currency: GBP,
+      value: 100,
+      deliveryBody: 'NE00',
       invoiceLines: []
     })
   })
