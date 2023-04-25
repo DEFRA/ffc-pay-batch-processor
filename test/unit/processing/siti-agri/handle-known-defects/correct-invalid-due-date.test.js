93a8447bac42e659a36e4442a996212d1d6c0ced
John Watson <42574977+johnwatson484@users.noreply.github.com>
2023-04-25 09:44:59 +0100

    Add flag for due date defect (#78)

--- test/unit/processing/siti-agri/handle-known-defects/correct-invalid-due-date.test.js
+++ test/unit/processing/siti-agri/handle-known-defects/correct-invalid-due-date.test.js
@@ -1,8 +1,15 @@
-const correctInvalidDueDate = require('../../../../../app/processing/siti-agri/handle-known-defects/correct-invalid-due-dates')
+const config = require('../../../../../app/config/processing')
+
 const { sfi } = require('../../../../../app/schemes')
 const invalidDueDates = require('../../../../../app/processing/siti-agri/handle-known-defects/correct-invalid-due-dates/invalid-due-dates')
 
+const correctInvalidDueDate = require('../../../../../app/processing/siti-agri/handle-known-defects/correct-invalid-due-dates')
+
 describe('Correct invalid due date', () => {
+  beforeEach(() => {
+    config.handleDueDateDefect = true
+  })
+
   test.each(invalidDueDates, 'remaps every possible invalid due date to the correct date', (invalidDueDate) => {
     const paymentRequest = {
       sourceSystem: sfi.sourceSystem,
@@ -24,6 +31,16 @@ describe('Correct invalid due date', () => {
   test.each(invalidDueDates, 'does not correct invalid due date if not SFI', (invalidDueDate) => {
     const paymentRequest = {
       sourceSystem: 'not SFI',
+      dueDate: invalidDueDate.invalid
+    }
+    const updatedPaymentRequest = correctInvalidDueDate(paymentRequest)
+    expect(updatedPaymentRequest.dueDate).toBe(invalidDueDate.invalid)
+  })
+
+  test.each(invalidDueDates, 'does not correct invalid due date if handle due date defect disabled', (invalidDueDate) => {
+    config.handleDueDateDefect = false
+    const paymentRequest = {
+      sourceSystem: sfi.sourceSystem,
       dueDate: invalidDueDate.invalid
     }
     const updatedPaymentRequest = correctInvalidDueDate(paymentRequest)
