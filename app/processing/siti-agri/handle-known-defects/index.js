45d10f9818f18ff8bbcbb6a5959ef59f54268355
Simon Dunn <simondunn90@gmail.com>
2023-05-15 15:26:38 +0100

    add to defects function

--- app/processing/siti-agri/handle-known-defects/index.js
+++ app/processing/siti-agri/handle-known-defects/index.js
@@ -1,8 +1,10 @@
+const { capBPSPenalties } = require('./cap-bps-penalties')
 const correctInvalidDueDate = require('./correct-invalid-due-dates')
 const removeDefunctParticipationPayment = require('./remove-defunct-participation-fund')
 
 const handleKnownDefects = (paymentRequest) => {
   paymentRequest = removeDefunctParticipationPayment(paymentRequest)
+  paymentRequest = capBPSPenalties(paymentRequest)
   return correctInvalidDueDate(paymentRequest)
 }
 
