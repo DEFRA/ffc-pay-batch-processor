aa50e0719bd16322b2e8329bdba67eaa8db9b930
Simon Dunn <simondunn90@gmail.com>
2023-05-17 10:34:38 +0100

    amend line-descriptions

--- app/constants/line-descriptions.js
+++ app/constants/line-descriptions.js
@@ -1,6 +1,6 @@
 module.exports = {
-  G00: 'G00 - Gross value of claim',
-  P01: 'P01 - Declaration penalty applied to Young Farmer',
-  P02: 'P02 - Over declaration penalty',
-  P04: 'P04 - Administrative penalty'
+  G00: 'G00',
+  P01: 'P01',
+  P02: 'P02',
+  P04: 'P04'
 }
