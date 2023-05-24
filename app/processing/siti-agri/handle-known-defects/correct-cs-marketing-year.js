a8aeba60f686f10e1e118daf8d3cfae524900650
John Watson <john.watson1@defra.gov.uk>
2023-05-24 14:05:50 +0100

    Add test

--- app/processing/siti-agri/handle-known-defects/correct-cs-marketing-year.js
+++ app/processing/siti-agri/handle-known-defects/correct-cs-marketing-year.js
@@ -6,10 +6,11 @@ const correctCSMarketingYear = (paymentRequest) => {
   }
   // Some CS payment requests only have a two digit marketing year.
   // CS started in 2016, so for any two digit year from 16 onwards, we need to prefix with 20
-  if (paymentRequest.marketingYear <= 16 || paymentRequest.marketingYear?.toString().length === 4) {
+  if (!paymentRequest.marketingYear || paymentRequest.marketingYear <= 16 || paymentRequest.marketingYear?.toString().length === 4) {
     return paymentRequest
   }
   paymentRequest.marketingYear = parseInt(`20${paymentRequest.marketingYear}`)
+  return paymentRequest
 }
 
 module.exports = {
---
+++ test/unit/processing/siti-agri/handle-known-defects/correct-cs-marketing-year.test.js
@@ -0,0 +1,36 @@
+const { cs, sfi } = require('../../../../../app/schemes')
+
+const { correctCSMarketingYear } = require('../../../../../app/processing/siti-agri/handle-known-defects/correct-cs-marketing-year')
+
+let paymentRequest
+
+describe('correct CS marketing year', () => {
+  beforeEach(() => {
+    paymentRequest = JSON.parse(JSON.stringify(require('../../../../mocks/payment-request').paymentRequest))
+    paymentRequest.sourceSystem = cs.sourceSystem
+  })
+
+  test('Should return unchanged payment request when scheme is not CS', () => {
+    paymentRequest.sourceSystem = sfi.sourceSystem
+    const result = correctCSMarketingYear(paymentRequest)
+    expect(result).toStrictEqual(paymentRequest)
+  })
+
+  test('Should return unchanged payment request when marketing year is less than 16', () => {
+    paymentRequest.marketingYear = 15
+    const result = correctCSMarketingYear(paymentRequest)
+    expect(result).toStrictEqual(paymentRequest)
+  })
+
+  test('Should return unchanged payment request when marketing year is already 4 digits', () => {
+    paymentRequest.marketingYear = 2016
+    const result = correctCSMarketingYear(paymentRequest)
+    expect(result).toStrictEqual(paymentRequest)
+  })
+
+  test('Should return unchanged payment request when marketing year is undefined', () => {
+    paymentRequest.marketingYear = undefined
+    const result = correctCSMarketingYear(paymentRequest)
+    expect(result).toStrictEqual(paymentRequest)
+  })
+})
