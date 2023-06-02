7456b521b75e154216e737d5dea38e47c51a3a04
Kirsten1822 <113036956+Kirsten1822@users.noreply.github.com>
2023-06-02 11:39:20 +0100

    181594/Remove-due-date-workaround (#86)
    * Update version in package.json
    
    * Remove due date workaround code
    
    * Removed more references to handleDueDateDefect

--- app/processing/siti-agri/handle-known-defects/index.js
+++ app/processing/siti-agri/handle-known-defects/index.js
@@ -1,13 +1,11 @@
 const removeDefunctParticipationPayment = require('./remove-defunct-participation-fund')
 const { capBPSPenalties } = require('./cap-bps-penalties')
 const { correctCSMarketingYear } = require('./correct-cs-marketing-year')
-const correctInvalidDueDate = require('./correct-invalid-due-dates')
 
 const handleKnownDefects = (paymentRequest) => {
   paymentRequest = removeDefunctParticipationPayment(paymentRequest)
   paymentRequest = capBPSPenalties(paymentRequest)
-  paymentRequest = correctCSMarketingYear(paymentRequest)
-  return correctInvalidDueDate(paymentRequest)
+  return correctCSMarketingYear(paymentRequest)
 }
 
 module.exports = handleKnownDefects
