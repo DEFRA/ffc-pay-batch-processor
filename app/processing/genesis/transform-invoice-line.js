6ea98033436286f9c54dbfe66facfe508ba7c976
John Watson <john.watson1@defra.gov.uk>
2023-06-23 12:40:24 +0100

    Transform invoice line

--- app/processing/genesis/transform-invoice-line.js
+++ app/processing/genesis/transform-invoice-line.js
@@ -1,57 +1,31 @@
-const { sfi, sfiPilot, lumpSums, bps, cs, fdmr } = require('../../constants/schemes')
-
 const transformInvoiceLine = (lineData, schemeId) => {
-  switch (schemeId) {
-    case sfi.schemeId:
-    case sfiPilot.schemeId:
-      return transformSFIInvoiceLine(lineData)
-    case lumpSums.schemeId:
-    case bps.schemeId:
-    case fdmr.schemeId:
-      return transformSitiInvoiceLine(lineData)
-    case cs.schemeId:
-      return transformCSInvoiceLine(lineData)
-    default:
-      throw new Error(`Unknown scheme: ${schemeId}`)
+  return {
+    invoiceNumber: lineData[1],
+    companyCodeEARFD: lineData[2],
+    costCentreEARFD: lineData[3],
+    objectiveCodeEARFD: lineData[4],
+    accountCodeEARFD: lineData[5],
+    subAccountCodeEARFD: lineData[6],
+    projectCodeEARFD: lineData[7],
+    valueEARFD: !isNaN(lineData[8]) ? parseFloat(lineData[8]) : undefined,
+    descriptionEARFD: lineData[9],
+    companyCodeDefra: lineData[10],
+    costCentreDefra: lineData[11],
+    objectiveCodeDefra: lineData[12],
+    accountCodeDefra: lineData[13],
+    subAccountCodeDefra: lineData[14],
+    projectCodeDefra: lineData[15],
+    valueDefra: !isNaN(lineData[16]) ? parseFloat(lineData[16]) : undefined,
+    descriptionDefra: lineData[17],
+    marketingYear: !isNaN(lineData[3]) ? parseInt(lineData[3]) : undefined,
+    schemeCode: lineData[4],
+    fundCode: lineData[5],
+    agreementNumber: lineData[6],
+    deliveryBody: lineData[7],
+    description: lineData[10],
+    dueDate: lineData[11],
+    accountCode: lineData[13]
   }
 }
-
-const transformSFIInvoiceLine = (lineData) => ({
-  invoiceNumber: lineData[1],
-  value: !isNaN(lineData[2]) ? parseFloat(lineData[2]) : undefined,
-  marketingYear: !isNaN(lineData[3]) ? parseInt(lineData[3]) : undefined,
-  schemeCode: lineData[4],
-  fundCode: lineData[5],
-  agreementNumber: lineData[6],
-  deliveryBody: lineData[7],
-  description: lineData[10],
-  dueDate: lineData[11],
-  accountCode: lineData[13]
-})
-
-const transformSitiInvoiceLine = (lineData) => ({
-  invoiceNumber: lineData[1],
-  value: !isNaN(lineData[2]) ? parseFloat(lineData[2]) : undefined,
-  marketingYear: !isNaN(lineData[3]) ? parseInt(lineData[3]) : undefined,
-  schemeCode: lineData[4],
-  fundCode: lineData[5],
-  deliveryBody: lineData[6],
-  description: lineData[8],
-  dueDate: lineData[9]
-})
-
-const transformCSInvoiceLine = (lineData) => ({
-  invoiceNumber: lineData[1],
-  value: !isNaN(lineData[2]) ? parseFloat(lineData[2]) : undefined,
-  marketingYear: !isNaN(lineData[3]) ? parseInt(lineData[3]) : undefined,
-  schemeCode: lineData[4],
-  fundCode: lineData[5],
-  agreementNumber: lineData[6],
-  deliveryBody: lineData[7],
-  convergence: lineData[8] === 'Y',
-  description: lineData[10],
-  dueDate: lineData[11],
-  accountCode: lineData[12]
-})
 
 module.exports = transformInvoiceLine
