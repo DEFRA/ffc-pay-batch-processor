52f351e9eefa3566d9d3da9bf964ce2525a04e8b
John Watson <john.watson1@defra.gov.uk>
2023-06-23 16:46:57 +0100

    Add account code

--- app/processing/genesis/schemas/invoice-line.js
+++ app/processing/genesis/schemas/invoice-line.js
@@ -4,6 +4,7 @@ module.exports = Joi.object({
   companyCode: Joi.string().required(),
   costCentre: Joi.string().required(),
   objectiveCode: Joi.string().required(),
+  accountCode: Joi.string().required(),
   subAccountCode: Joi.string().required(),
   projectCode: Joi.string().required(),
   value: Joi.number().required(),
