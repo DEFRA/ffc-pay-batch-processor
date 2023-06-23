cecb1e94b59a4ecc2c374586d033b076792763af
John Watson <john.watson1@defra.gov.uk>
2023-06-23 16:12:54 +0100

    Delete redundant module

--- app/processing/genesis/is-net-line.js
+++
@@ -1,9 +0,0 @@
-const { N00 } = require('../../constants/line-codes')
-
-const isNetLine = (invoiceLine) => {
-  return invoiceLine.description.startsWith(N00)
-}
-
-module.exports = {
-  isNetLine
-}
