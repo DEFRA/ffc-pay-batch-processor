3700ba3b173b202f4061a11fd1c4ae0f66b850be
John Watson <john.watson1@defra.gov.uk>
2023-06-27 11:45:51 +0100

    Fixes

--- app/processing/genesis/schemas/invoice-line.js
+++ app/processing/genesis/schemas/invoice-line.js
@@ -3,10 +3,10 @@ const Joi = require('joi')
 module.exports = Joi.object({
   companyCode: Joi.string().required(),
   costCentre: Joi.string().required(),
-  objectiveCode: Joi.string().required(),
+  standardCode: Joi.string().required(),
   accountCode: Joi.string().required(),
   subAccountCode: Joi.string().required(),
-  projectCode: Joi.string().required(),
+  projectCode: Joi.string().optional(),
   value: Joi.number().required(),
   description: Joi.string().required()
 })
--- app/processing/genesis/transform-invoice-line.js
+++ app/processing/genesis/transform-invoice-line.js
@@ -2,19 +2,19 @@ const transformInvoiceLine = (lineData, schemeId) => {
   return [{
     companyCode: lineData[2],
     costCentre: lineData[3],
-    objectiveCode: lineData[4],
+    standardCode: lineData[4],
     accountCode: lineData[5],
     subAccountCode: lineData[6],
-    projectCode: lineData[7],
+    projectCode: lineData[7].length > 0 ? lineData[7] : undefined,
     value: !isNaN(lineData[8]) ? parseFloat(lineData[8]) : undefined,
     description: lineData[9]
   }, {
     companyCode: lineData[10],
     costCentre: lineData[11],
-    objectiveCode: lineData[12],
+    standardCode: lineData[12],
     accountCode: lineData[13],
     subAccountCode: lineData[14],
-    projectCode: lineData[15],
+    projectCode: lineData[15].length > 0 ? lineData[15] : undefined,
     value: !isNaN(lineData[16]) ? parseFloat(lineData[16]) : undefined,
     description: lineData[17]
   }]
