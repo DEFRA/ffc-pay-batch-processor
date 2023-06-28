312ead17ae726b726f0928aeb5ca1aa11704a089
John Watson <john.watson1@defra.gov.uk>
2023-06-28 11:36:28 +0100

    Add tests

--- test/unit/processing/genesis/transform-invoice-lines.test.js
+++ test/unit/processing/genesis/transform-invoice-lines.test.js
@@ -4,17 +4,24 @@ describe('transform genesis invoice lines', () => {
   test('transforms invoice line', async () => {
     const lineData = ['D', '1096514', '32', '40121', '01372', '0197', '221', '', '10767.74', 'Payment for ESS P1 to P2 Transfer Other objective in North West', '', '', '', '', '', '', '', '', '']
     const result = transformInvoiceLine(lineData)
-    expect(result).toEqual({
-      invoiceNumber: 'SFI0000001',
-      value: 100,
-      marketingYear: 2022,
-      schemeCode: '80001',
-      fundCode: 'DRD10',
-      agreementNumber: 'SIP00000000001',
-      deliveryBody: 'RP00',
-      description: 'G00 - Gross value of claim',
-      dueDate: '2022-12-01',
-      accountCode: 'SOS273'
-    })
+    expect(result).toEqual([{
+      accountCode: '0197',
+      companyCode: '32',
+      costCentre: '40121',
+      description: 'Payment for ESS P1 to P2 Transfer Other objective in North West',
+      projectCode: undefined,
+      standardCode: '01372',
+      subAccountCode: '221',
+      value: 10767.74
+    }, {
+      accountCode: '',
+      companyCode: '',
+      costCentre: '',
+      description: '',
+      projectCode: undefined,
+      standardCode: '',
+      subAccountCode: '',
+      value: NaN
+    }])
   })
 })
