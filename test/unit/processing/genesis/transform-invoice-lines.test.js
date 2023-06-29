6fce1f6651bcefbc96855046e4a5a21e157b303d
John Watson <john.watson1@defra.gov.uk>
2023-06-29 11:41:40 +0100

    Added test

--- test/unit/processing/genesis/transform-invoice-lines.test.js
+++ test/unit/processing/genesis/transform-invoice-lines.test.js
@@ -1,7 +1,7 @@
 const transformInvoiceLine = require('../../../../app/processing/genesis/transform-invoice-line')
 
 describe('transform genesis invoice lines', () => {
-  test('transforms invoice line', async () => {
+  test('transforms invoice line when only one co-financing element', async () => {
     const lineData = ['D', '1096514', '32', '40121', '01372', '0197', '221', '', '10767.74', 'Payment for ESS P1 to P2 Transfer Other objective in North West', '', '', '', '', '', '', '', '', '']
     const result = transformInvoiceLine(lineData)
     expect(result).toEqual([{
@@ -22,6 +22,30 @@ describe('transform genesis invoice lines', () => {
       standardCode: '',
       subAccountCode: '',
       value: NaN
+    }])
+  })
+
+  test('transforms invoice line when only both co-financing elements', async () => {
+    const lineData = ['D', '1096514', '32', '40121', '01372', '0197', '221', '', '10767.74', 'Payment for ESS P1 to P2 Transfer Other objective in North West', '32', '40121', '01372', '0197', '221', '', '10767.74', 'Payment for ESS P1 to P2 Transfer Other objective in North West']
+    const result = transformInvoiceLine(lineData)
+    expect(result).toEqual([{
+      accountCode: '0197',
+      companyCode: '32',
+      costCentre: '40121',
+      description: 'Payment for ESS P1 to P2 Transfer Other objective in North West',
+      projectCode: undefined,
+      standardCode: '01372',
+      subAccountCode: '221',
+      value: 10767.74
+    }, {
+      accountCode: '0197',
+      companyCode: '32',
+      costCentre: '40121',
+      description: 'Payment for ESS P1 to P2 Transfer Other objective in North West',
+      projectCode: undefined,
+      standardCode: '01372',
+      subAccountCode: '221',
+      value: 10767.74
     }])
   })
 })
