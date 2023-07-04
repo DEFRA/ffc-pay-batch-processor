e191fa310de925d10aa3ee27e4f4bf7274e2a92e
Simon Dunn <simondunn90@gmail.com>
2023-07-04 16:02:46 +0100

    add retry function

--- app/storage.js
+++ app/storage.js
@@ -45,7 +45,7 @@ const getInboundFileList = async () => {
 
   const fileList = []
   for await (const file of container.listBlobsFlat({ prefix: config.inboundFolder })) {
-    if (file.name.endsWith('.dat') || file.name.endsWith('.gne')) {
+    if (file.name.endsWith('.dat') || file.name.endsWith('.gne') || file.name.endsWith('.ctl')) {
       fileList.push(file.name.replace(`${config.inboundFolder}/`, ''))
     }
   }
@@ -85,11 +85,20 @@ const quarantinePaymentFile = async (filename, quarantineFilename) => {
   return moveFile(config.inboundFolder, config.quarantineFolder, filename, quarantineFilename)
 }
 
+const getFile = async (filename) => {
+  console.log(`Searching for ${filename}`)
+  const blob = await getBlob(config.inboundFolder, filename)
+  const downloaded = await blob.downloadToBuffer()
+  console.log(`Found ${filename}`)
+  return downloaded.toString()
+}
+
 module.exports = {
   getInboundFileList,
   getInboundFileDetails,
   downloadPaymentFile,
   archivePaymentFile,
   quarantinePaymentFile,
-  blobServiceClient
+  blobServiceClient,
+  getFile
 }
