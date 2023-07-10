913f54d111bf76a1fc3450834b71f1b40ec349ed
Kirsten1822 <kirsten.williamson@kainos.com>
2023-07-10 13:52:00 +0100

    Please work

--- app/processing/glos/get-payment-requests.js
+++ app/processing/glos/get-payment-requests.js
@@ -1,3 +1,4 @@
+const { fc } = require('../../constants/schemes')
 const transformBatch = require('./transform-batch')
 const transformHeader = require('./transform-header')
 const transformInvoiceLine = require('./transform-invoice-line')
@@ -7,13 +8,22 @@ const validateBatch = require('./validate-batch')
 const readGlosFile = async (readBatchLines, scheme, input, filename) => {
   return new Promise((resolve, reject) => {
     const batch = createBatch()
+    const batchLines = []
     readBatchLines.on('line', (line) => {
       const batchLine = line.split(',')
+      batchLines.push(transformLines(batchLine, filename))
+      console.log(batchLines)
+      /*
       !readLine(batchLine, batch, scheme, filename) &&
         reject(new Error('Invalid file - Unknown line'))
+      */
     })
 
     readBatchLines.on('close', () => {
+      groupByInvoiceNumber(batchLines)
+
+      console.log(batchLines)
+
       validateBatch(batch.batchHeaders, batch.paymentRequests)
         ? resolve({ paymentRequestsCollection: filterPaymentRequests(batch.paymentRequests, scheme.sourceSystem), batchExportDate: batch.batchHeaders[0]?.exportDate })
         : reject(new Error('Invalid file'))
@@ -30,24 +40,41 @@ const createBatch = () => {
   }
 }
 
-const readLine = (batchLine, batch, scheme, filename) => {
-  const lineType = batchLine[0]
-
-  switch (lineType) {
-    case 'B':
-      batch.batchHeaders.push(transformBatch(batchLine))
-      return true
-    case 'H':
-      batch.paymentRequests.push(transformHeader(batchLine, scheme.schemeId, filename))
-      return true
-    case 'L':
-      batch.paymentRequests[batch.paymentRequests.length - 1]
-        .invoiceLines
-        .push(transformInvoiceLine(batchLine, scheme.schemeId))
-      return true
-    default:
-      return false
+const transformLines = (batchLine, filename) => {
+  return {
+    sourceSystem: fc.sourceSystem,
+    batch: filename,
+    invoiceNumber: batchLine[7],
+    paymentRequestNumber: 1,
+    frn: batchLine[21],
+    sbi: batchLine[24],
+    claimDate: batchLine[23],
+    standardCode: batchLine[12],
+    description: batchLine[11],
+    value: batchLine[8]
   }
+}
+
+const groupByInvoiceNumber = (batchLines) => {
+  return [...batchLines.reduce((x, y) => {
+    const key = y.invoiceNumber
+
+    // if key doesn't exist then first instance so create new group
+    const item = x.get(key) || Object.assign({}, {
+      sourceSystem: y.sourceSystem,
+      batch: y.batch,
+      invoiceNumber: y.invoiceNumber,
+      paymentRequestNumber: y.paymentRequestNumber,
+      frn: y.frn,
+      sbi: y.sbi,
+      claimDate: y.claimDate,
+      standardCode: y.standardCode,
+      description: y.description,
+      value: y.value
+    })
+
+    return x.set(key, item)
+  }, new Map()).values()]
 }
 
 module.exports = readGlosFile
