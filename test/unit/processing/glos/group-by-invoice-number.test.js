46c95af4dc5b2b942a12414794b783d29bc0af26
Kirsten1822 <kirsten.williamson@kainos.com>
2023-07-17 15:44:40 +0100

    Suggested changes

--- test/unit/processing/glos/group-by-invoice-number.test.js
+++ test/unit/processing/glos/group-by-invoice-number.test.js
@@ -4,31 +4,27 @@ const { filename1 } = require('../../../mocks/glos-filenames')
 const groupByInvoiceNumber = require('../../../../app/processing/glos/group-by-invoice-number')
 
 describe('group by invoice number', () => {
+  const glosPaymentRequest = {
+    correlationId: mockCorrelationId,
+    batch: filename1,
+    batchExportDate: '31/05/2023 22:01:38',
+    invoiceNumber: '33315 16',
+    paymentRequestNumber: 1,
+    frn: '1102294241',
+    sbi: '106609512',
+    claimDate: '31/05/2023 22:01:38',
+    standardCode: '028Z141Q',
+    description: 'English Woodland Grant Scheme',
+    value: 81
+  }
+
   test('should group by invoice number with 2 invoice lines', () => {
     const paymentRequests = [{
-      correlationId: mockCorrelationId,
-      batch: filename1,
-      batchExportDate: '31/05/2023 22:01:38',
-      invoiceNumber: '33315 16',
-      paymentRequestNumber: 1,
-      frn: '1102294241',
-      sbi: '106609512',
-      claimDate: '31/05/2023 22:01:38',
-      standardCode: '028Z141Q',
-      description: 'English Woodland Grant Scheme',
-      value: 81
+      ...glosPaymentRequest,
+      invoiceNumber: '33315 16'
     }, {
-      correlationId: mockCorrelationId,
-      batch: filename1,
-      batchExportDate: '31/05/2023 22:01:38',
-      invoiceNumber: '33315 16',
-      paymentRequestNumber: 1,
-      frn: '1102294241',
-      sbi: '106609512',
-      claimDate: '31/05/2023 22:01:38',
-      standardCode: '028Z141Q',
-      description: 'English Woodland Grant Scheme',
-      value: 32
+      ...glosPaymentRequest,
+      invoiceNumber: '33315 16'
     }]
 
     const groupedPayments = groupByInvoiceNumber(paymentRequests)
@@ -38,53 +34,17 @@ describe('group by invoice number', () => {
 
   test('should group by invoice number with multiple unique invoice numbers', () => {
     const paymentRequests = [{
-      correlationId: mockCorrelationId,
-      batch: filename1,
-      batchExportDate: '31/05/2023 22:01:38',
-      invoiceNumber: '33315 16',
-      paymentRequestNumber: 1,
-      frn: '1102294241',
-      sbi: '106609512',
-      claimDate: '31/05/2023 22:01:38',
-      standardCode: '028Z141Q',
-      description: 'English Woodland Grant Scheme',
-      value: 81
+      ...glosPaymentRequest,
+      invoiceNumber: '33315 16'
     }, {
-      correlationId: mockCorrelationId,
-      batch: filename1,
-      batchExportDate: '31/05/2023 22:01:38',
-      invoiceNumber: '33315 16',
-      paymentRequestNumber: 1,
-      frn: '1102294241',
-      sbi: '106609512',
-      claimDate: '31/05/2023 22:01:38',
-      standardCode: '028Z141Q',
-      description: 'English Woodland Grant Scheme',
-      value: 32
+      ...glosPaymentRequest,
+      invoiceNumber: '33315 16'
     }, {
-      correlationId: mockCorrelationId,
-      batch: filename1,
-      batchExportDate: '31/05/2023 22:01:38',
-      invoiceNumber: '23747 13',
-      paymentRequestNumber: 1,
-      frn: '1102294241',
-      sbi: '106609512',
-      claimDate: '31/05/2023 22:01:38',
-      standardCode: '028Z141Q',
-      description: 'English Woodland Grant Scheme',
-      value: 32
+      ...glosPaymentRequest,
+      invoiceNumber: '23747 13'
     }, {
-      correlationId: mockCorrelationId,
-      batch: filename1,
-      batchExportDate: '31/05/2023 22:01:38',
-      invoiceNumber: '33315 16',
-      paymentRequestNumber: 1,
-      frn: '1102294241',
-      sbi: '106609512',
-      claimDate: '31/05/2023 22:01:38',
-      standardCode: '028Z141Q',
-      description: 'English Woodland Grant Scheme',
-      value: 32
+      ...glosPaymentRequest,
+      invoiceNumber: '33315 16'
     }]
 
     const groupedPayments = groupByInvoiceNumber(paymentRequests)
