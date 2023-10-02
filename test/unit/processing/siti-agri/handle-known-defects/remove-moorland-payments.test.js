da3385e1f07a51891238aad5f248c1f86a3513d0
Simon Dunn <simondunn90@gmail.com>
2023-10-02 14:12:46 +0100

    unit tests

--- test/unit/processing/siti-agri/handle-known-defects/remove-moorland-payments.test.js
+++ test/unit/processing/siti-agri/handle-known-defects/remove-moorland-payments.test.js
@@ -38,11 +38,41 @@ describe('Remove moorland payments', () => {
     expect(updatedPaymentRequest.invoiceLines.find(invoiceLine => invoiceLine.schemeCode === moorlandSchemeCode).value).toBe(0)
   })
 
-  test('removes moorland payment when sourceSystem is SFI22 and two invoice lines with 0 total', () => {
+  test('removes moorland payment when sourceSystem is SFI22 and other invoice lines have a net zero total', () => {
     paymentRequest.invoiceLines = [{ ...invoiceLine, value: 500 }, { ...invoiceLine, value: -500 }, moorlandInvoiceLine]
     const updatedPaymentRequest = removeMoorlandPayments(paymentRequest)
     expect(updatedPaymentRequest.value).toBe(0)
     expect(updatedPaymentRequest.invoiceLines.find(invoiceLine => invoiceLine.schemeCode === moorlandSchemeCode).value).toBe(0)
+  })
+
+  test('removes moorland payment when sourceSystem is SFI22 and other invoice lines have a multiple reductions', () => {
+    paymentRequest.invoiceLines = [{ ...invoiceLine, value: 500 }, { ...invoiceLine, value: -250 }, { ...invoiceLine, value: -250, descripton: 'P24 - Over declaration reduction' }, moorlandInvoiceLine]
+    const updatedPaymentRequest = removeMoorlandPayments(paymentRequest)
+    expect(updatedPaymentRequest.value).toBe(0)
+    expect(updatedPaymentRequest.invoiceLines.find(invoiceLine => invoiceLine.schemeCode === moorlandSchemeCode).value).toBe(0)
+  })
+
+  test('removes moorland payment if multiple groups net values all zero', () => {
+    paymentRequest.invoiceLines.push({ ...invoiceLine, value: 500, schemeCode: '80002' })
+    paymentRequest.invoiceLines.push({ ...invoiceLine, value: -500, schemeCode: '80002' })
+    const updatedPaymentRequest = removeMoorlandPayments(paymentRequest)
+    expect(updatedPaymentRequest.value).toBe(0)
+    expect(updatedPaymentRequest.invoiceLines.find(invoiceLine => invoiceLine.schemeCode === moorlandSchemeCode).value).toBe(0)
+  })
+
+  test('removes moorland payment if decimal values', () => {
+    paymentRequest.invoiceLines = [{ ...invoiceLine, value: 500.10 }, { ...invoiceLine, value: -500.10 }, moorlandInvoiceLine]
+    const updatedPaymentRequest = removeMoorlandPayments(paymentRequest)
+    expect(updatedPaymentRequest.value).toBe(0)
+    expect(updatedPaymentRequest.invoiceLines.find(invoiceLine => invoiceLine.schemeCode === moorlandSchemeCode).value).toBe(0)
+  })
+
+  test('does not remove moorland payment when sourceSystem is SFI22 and other gross value', () => {
+    paymentRequest.invoiceLines = [moorlandInvoiceLine, { ...invoiceLine, value: 500 }]
+    paymentRequest.value = 765
+    const updatedPaymentRequest = removeMoorlandPayments(paymentRequest)
+    expect(updatedPaymentRequest.value).toBe(765)
+    expect(updatedPaymentRequest.invoiceLines.find(invoiceLine => invoiceLine.schemeCode === moorlandSchemeCode).value).toBe(265)
   })
 
   test('does not remove moorland payment when sourceSystem is SFI23', () => {
