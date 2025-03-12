6e0012defd92786f89c487ee397e4c139335b491
Oliver Lewington <oliver.lewington@eviden.com>
2025-03-12 16:07:47 +0000

    Adding feature flag to disable service (#152)
    * Adding feature flag to disable service
    
    * 1.41.3
    
    ---------
    
    Co-authored-by: ffcplatform <ffcplatforms@environment-agency.gov.uk>

--- app/config/processing.js
+++ app/config/processing.js
@@ -2,6 +2,7 @@ const Joi = require('joi')
 
 // Define config schema
 const schema = Joi.object({
+  processingActive: Joi.boolean().default(true),
   pollingInterval: Joi.number().default(10000), // 10 seconds
   maxProcessingTries: Joi.number().default(3),
   disableSequenceValidation: Joi.boolean().default(false),
@@ -10,6 +11,7 @@ const schema = Joi.object({
 
 // Build config
 const config = {
+  processingActive: process.env.PROCESSING_ACTIVE,
   pollingInterval: process.env.POLLING_INTERVAL,
   maxProcessingTries: process.env.MAX_PROCESSING_TRIES,
   disableSequenceValidation: process.env.DISABLE_SEQUENCE_VALIDATION,
--- app/index.js
+++ app/index.js
@@ -1,7 +1,19 @@
 require('./insights').setup()
 require('log-timestamp')
+
+const processingConfig = require('./config/processing')
 const processing = require('./processing')
 
-module.exports = (async () => {
-  await processing.start()
+const startApp = async () => {
+  if (processingConfig.processingActive) {
+    await processing.start()
+  } else {
+    console.info('Processing capabilities are currently not enabled in this environment')
+  }
+}
+
+(async () => {
+  await startApp()
 })()
+
+module.exports = startApp
