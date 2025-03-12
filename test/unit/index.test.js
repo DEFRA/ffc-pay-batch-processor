5f9947d7ba2c2415bbaab90cb594c5c5e8f77adc
Oliver Lewington <oliver.lewington@eviden.com>
2025-03-12 15:32:06 +0000

    Adding feature flag to disable service

---
+++ test/unit/index.test.js
@@ -0,0 +1,42 @@
+const processingConfig = require('../../app/config/processing')
+
+jest.mock('../../app/processing')
+const { start: mockStartMessaging } = require('../../app/processing')
+
+const startApp = require('../../app')
+
+describe('app start', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('starts processing when active is true', async () => {
+    processingConfig.processingActive = true
+    await startApp()
+    expect(mockStartMessaging).toHaveBeenCalledTimes(1)
+  })
+
+  test('does not start processing if active is false', async () => {
+    processingConfig.processingActive = false
+    await startApp()
+    expect(mockStartMessaging).toHaveBeenCalledTimes(0)
+  })
+
+  test('does not log console.info when active is true', async () => {
+    processingConfig.processingActive = true
+    const consoleInfoSpy = jest.spyOn(console, 'info').mockImplementation(() => {})
+    await startApp()
+    expect(consoleInfoSpy).not.toHaveBeenCalled()
+    consoleInfoSpy.mockRestore()
+  })
+
+  test('logs console.info when active is false', async () => {
+    processingConfig.processingActive = false
+    const consoleInfoSpy = jest.spyOn(console, 'info').mockImplementation(() => {})
+    await startApp()
+    expect(consoleInfoSpy).toHaveBeenCalledWith(
+      expect.stringContaining('Processing capabilities are currently not enabled in this environment')
+    )
+    consoleInfoSpy.mockRestore()
+  })
+})
