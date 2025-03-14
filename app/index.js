35a8685bf4ad708e3bfeb51b044cd642454da5a3
Oliver Lewington <oliver.lewington@eviden.com>
2025-03-14 15:43:26 +0000

    Adding liveness check (#153)
    * Adding liveness check
    
    * update package-lock.json
    
    * 1.41.4
    
    ---------
    
    Co-authored-by: ffcplatform <ffcplatforms@environment-agency.gov.uk>

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 require('./insights').setup()
 require('log-timestamp')
 
-const processingConfig = require('./config/processing')
+const { processingConfig } = require('./config')
 const processing = require('./processing')
 
+const { start: startServer } = require('./server')
+
 const startApp = async () => {
+  await startServer()
   if (processingConfig.processingActive) {
     await processing.start()
   } else {
