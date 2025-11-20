fd301bdf266e03be8644867bf604aa39b175b73c
Oliver Lewington <oliver.lewington@atos.net>
2025-11-20 11:51:30 +0000

    Removing code changes

--- app/processing/siti-agri/validate-batch.js
+++ app/processing/siti-agri/validate-batch.js
@@ -2,9 +2,7 @@ const { convertToPence, getTotalValueInPence } = require('../../currency-convert
 const schema = require('./schemas/batch-header')
 
 const validateBatch = (batchHeader, paymentRequests) => {
-  if (batchHeader.length !== 1) {
-    return false
-  }
+  if (batchHeader.length !== 1) return false
 
   const validSchema = isValidSchema(batchHeader[0])
   const numberOfPaymentRequestsValid = batchHeader[0].numberOfPaymentRequests === paymentRequests.length
